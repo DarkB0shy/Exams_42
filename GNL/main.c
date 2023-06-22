89d42eb70a1e259f261dad214b02dd4353acb7be
Darkb0shy <dcarassi@localhost.localdomain>
2023-06-22 11:09:24 +0200

    GNL?

---
+++ GNL/main.c
@@ -0,0 +1,17 @@
+#include "get_next_line.h"
+
+int main (int argc, char **argv)
+{
+  if (argc != 2)
+    return (1 * printf("Utilizzo: ./a.out <FILE_DA_LEGGERE>"));
+  int fd = open(argv[1], O_RDONLY);
+  char  *line = get_next_line(fd);
+  while (line)
+  {
+    printf("%s", line);
+    free(line);
+    line = get_next_line(fd);
+  }
+  close (fd);
+  return (0);
+}
