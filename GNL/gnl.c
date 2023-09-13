10bbd2f9f00b887b52f426556cc7f083ca73baeb
Darkb0shy <dcarassi@localhost.localdomain>
2023-09-13 11:19:09 +0200

    Johnny Jab

--- GNL/gnl.c
+++ GNL/gnl.c
@@ -4,10 +4,9 @@ char    *gnl(int fd)
 {
   static char   buffer[B_S];
   static int    buffer_pos = -1;
-  static int    max_buffer_pos;
-  char          *line = NULL;
-  char          *tmp_line;
-  int           line_pos = 0;
+  static int    max_buffer_pos = 0;
+  char  *line = NULL;
+  int   line_pos = 0;
 
   if (B_S < 1 || fd < 0 || fd >= 1024)
     return (NULL);
@@ -18,7 +17,7 @@ char    *gnl(int fd)
   }
   if (max_buffer_pos < 1)
     return (NULL);
-  line = malloc(sizeof(char)*(max_buffer_pos + 1));
+  line = malloc (sizeof(char) * (max_buffer_pos + 1));
   line[line_pos] = '\0';
   while (buffer_pos < max_buffer_pos)
   {
@@ -29,10 +28,31 @@ char    *gnl(int fd)
     {
       max_buffer_pos = read(fd, &buffer, B_S);
       buffer_pos = 0;
-      line[line_pos] = '\0';
-      return (line);
+      break;
     }
   }
   line[line_pos] = '\0';
   return (line);
+}
+
+int main (int argc, char **argv)
+{
+  if (argc != 2)
+    printf("Insert the file to read line per line\n");
+  else
+  { 
+    int fd = open(argv[1], O_RDONLY);
+    char *line = gnl(fd);
+    while (line)
+    {
+      printf("%s", line);
+      free(line);
+      line = gnl(fd);
+    }
+    free(line);
+    close(fd);
+
+  }
+  printf("\n");
+  return (0);
 }
--- GNL/gnl.h
+++ GNL/gnl.h
@@ -1,14 +1,14 @@
 #ifndef GNL_H
-# define GNL_H
+#define GNL_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
 
-# ifndef B_S
-# define B_S 42
-# endif
+#ifndef B_S
+#define B_S 42
+#endif
 
 char    *gnl(int fd);
 
